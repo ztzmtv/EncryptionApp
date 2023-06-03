8d6aa0e0d56b41d6cd601db8043f09a580ac3adb
Azat Azmetov <azmetoff@gmail.com>
2023-06-03 22:03:01 +0300

    crypto manager works

--- app/src/main/java/com/azmetov/encryptionapp/MainActivity.kt
+++ app/src/main/java/com/azmetov/encryptionapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.azmetov.encryptionapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,10 +12,30 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.azmetov.encryptionapp.ui.theme.EncryptionAppTheme
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val testMessage = "testMessage"
+        val cryptoManager = CryptoManager()
+
+        val bytes = testMessage.encodeToByteArray()
+        val file = File(filesDir, "secret.txt")
+        if (!file.exists())
+            file.createNewFile()
+        val fos = FileOutputStream(file)
+        val fis = FileInputStream(file)
+
+        val encoded = cryptoManager.encrypt(bytes, fos).decodeToString()
+        Log.d("TAG encoded", encoded)
+        val decoded = cryptoManager.decrypt(fis).decodeToString()
+        Log.d("TAG decoded", decoded)
+
+
         setContent {
             EncryptionAppTheme {
                 // A surface container using the 'background' color from the theme
